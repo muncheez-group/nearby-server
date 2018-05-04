bcc8474dc86b8ddb501b41fb7cb5716c640e66ed
lhmun <louis.mun@gmail.com>
2018-05-03 17:09:16 -0700

    Added tests for data generator

---
+++ __tests__/generate-nearby.test.js
@@ -0,0 +1,54 @@
+const faker = require('faker');
+const count = 5;
+let generateTest = (total) => {
+	let result = [];
+	for (let i = 0; i < total; i++) {
+		const data = {
+			name: faker.address.city() + faker.address.streetName(),
+			place_id: i,
+			google_rating: ((Math.random() * 4) + 1).toFixed(1),
+			zagat_rating: ((Math.random() * 4) + 1).toFixed(1),
+			photos: [Math.floor(Math.random() * 100),
+				Math.floor(Math.random() * 100), Math.floor(Math.random() * 100),
+				Math.floor(Math.random() * 100)],
+			neighborhood: faker.address.streetName() + faker.address.citySuffix(),
+			price_level: Math.floor(((Math.random() * 4) + 1)),
+			types: faker.lorem.word(),
+			nearby: [Math.floor(Math.random() * total),
+				Math.floor(Math.random() * total), Math.floor(Math.random() * total),
+				Math.floor(Math.random() * total), Math.floor(Math.random() * total),
+				Math.floor(Math.random() * total)],
+		};
+		result.push(data)
+	}
+	return result;
+};
+
+describe ('data generator', () => {
+	it ('has correct properties', () => {
+		for (let i = 0; i < count; i++) {
+			expect(generateTest(count)[i]).toHaveProperty('name');
+			expect(generateTest(count)[i]).toHaveProperty('place_id');
+			expect(generateTest(count)[i]).toHaveProperty('google_rating');
+			expect(generateTest(count)[i]).toHaveProperty('zagat_rating');
+			expect(generateTest(count)[i]).toHaveProperty('photos');
+			expect(generateTest(count)[i]).toHaveProperty('neighborhood');
+			expect(generateTest(count)[i]).toHaveProperty('price_level');
+			expect(generateTest(count)[i]).toHaveProperty('types');
+			expect(generateTest(count)[i]).toHaveProperty('nearby');
+		}
+	});
+	it ('has valid property types', () => {
+		for (let i = 0; i < count; i++) {
+			expect(typeof generateTest(count)[i].name === String);
+			expect(generateTest(count)[i].place_id === Number);
+			expect(generateTest(count)[i].google_rating <= 4 && generateTest(count)[i].google_rating > 0);
+			expect(generateTest(count)[i].zagat_rating <= 4 && generateTest(count)[i].zagat_rating > 0);
+			expect(typeof generateTest(count)[i].photos === Object);
+			expect(typeof generateTest(count)[i].neighborhood === String);
+			expect(generateTest(count)[i].price_level <= 4 && generateTest(count)[i].price_level > 0);
+			expect(typeof generateTest(count)[i].types === String);
+			expect(typeof generateTest(count)[i].nearby === Object);
+		}
+	})
+});
--- generate-nearby.js
+++ generate-nearby.js
@@ -1,41 +1,45 @@
-let faker = require('faker');
-let fs = require('fs');
-let data = [];
+const faker = require('faker');
+const fs = require('fs');
+
 const total = 10000000;
 console.log('starting');
-let generate = (writer, encoding, callback) => {
+const generate = (writer, encoding, callback) => {
   let i = total;
-  let write = () => {
+  const write = () => {
     let ok = true;
     do {
-      let data = {
+      const data = {
         name: faker.address.city() + faker.address.streetName(),
         place_id: i,
-        google_rating: (Math.random() * 4 + 1).toFixed(1),
-        zagat_rating: (Math.random() * 4 + 1).toFixed(1),
-        photos: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        neighborhood: faker.address.streetName() + faker.address.citySuffix(), 
-        price_level: Math.floor((Math.random() * 4 + 1)),
+        google_rating: ((Math.random() * 4) + 1).toFixed(1),
+        zagat_rating: ((Math.random() * 4) + 1).toFixed(1),
+        photos: [Math.floor(Math.random() * 100),
+          Math.floor(Math.random() * 100), Math.floor(Math.random() * 100),
+          Math.floor(Math.random() * 100)],
+        neighborhood: faker.address.streetName() + faker.address.citySuffix(),
+        price_level: Math.floor(((Math.random() * 4) + 1)),
         types: faker.lorem.word(),
-        nearby: [Math.floor(Math.random() * total), Math.floor(Math.random() * total), Math.floor(Math.random() * total),
-          Math.floor(Math.random() * total), Math.floor(Math.random() * total), Math.floor(Math.random() * total)]
+        nearby: [Math.floor(Math.random() * total),
+          Math.floor(Math.random() * total), Math.floor(Math.random() * total),
+          Math.floor(Math.random() * total), Math.floor(Math.random() * total),
+          Math.floor(Math.random() * total)],
       };
-      i--;
+      i -= 1;
       if (i === 0) { // end
-        writer.write(JSON.stringify(data) + ']', encoding, callback);
+        writer.write(`${JSON.stringify(data)}]`, encoding, callback);
       } else if (i === total - 1) { // start
-        ok = writer.write('[' + JSON.stringify(data) + ',', encoding);
+        ok = writer.write(`[${JSON.stringify(data)},`, encoding);
       } else {
-        ok = writer.write(JSON.stringify(data)+',');
+        ok = writer.write(`${JSON.stringify(data)},`);
       }
     } while (i > 0 && ok);
     if (i > 0) {
       writer.once('drain', write);
     }
-  }
+  };
   write();
-}
-generate (fs.createWriteStream('data.json','utf8', () => {
+};
+generate(fs.createWriteStream('data.json', 'utf8', () => {
   console.log('Finished!');
 }));
 
