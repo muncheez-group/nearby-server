44e5fa45f0da10683df2bd7a87be6adb7643f438
lhmun <louis.mun@gmail.com>
2018-05-15 17:00:22 -0700

    Changed some webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,37 @@
-var path = require('path');
+
+
+// module.exports = {
+//   entry: `${SRC_DIR}/app.jsx`,
+//   output: {
+//     filename: 'bundle.js',
+//     path: DIST_DIR
+//   },
+//   module : {
+//     loaders : [
+//       {
+//         test : /\.jsx?/,
+//         include : SRC_DIR,
+//         loader : 'babel-loader',      
+//         query: {
+//           presets: ['react', 'es2015']
+//        }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader','css-loader']
+//       }
+//     ]
+//   }
+// };
+
+var path = require('path')
+var webpack = require('webpack')
+var nodeExternals = require('webpack-node-externals')
+
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
-module.exports = {
+var browserConfig = {
   entry: `${SRC_DIR}/app.jsx`,
   output: {
     filename: 'bundle.js',
@@ -24,4 +53,27 @@ module.exports = {
       }
     ]
   }
-};
+}
+
+var serverConfig = {
+  entry: __dirname + '/server/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: __dirname,
+    filename: 'server.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "false"
+    })
+  ]
+}
+
+module.exports = [browserConfig, serverConfig]
