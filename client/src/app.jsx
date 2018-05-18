ee3125a803bf0efed49c19bfa8d5c4ab82f76bfb
Ubuntu <ubuntu@ip-172-31-2-129.us-west-1.compute.internal>
2018-05-18 23:19:32 +0000

    Working instance

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -5,36 +5,64 @@ import Footer from './components/Footer.jsx';
 import $ from 'jquery';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
     this.state = {
       currentRestaurant: this.props.currentRestaurant || {},
       nearbyRestaurants: this.props.nearbyRestaurants || [],
       checkID: true
     }
-	}
+  }
+
+  componentDidMount() {
+    this._getData();
+  }
+
+  _getData() {
+    var id = window.location.href.split('/')[4];
+    if (window.location.href.split('/')[4] !== undefined) {
+      $.ajax({
+        url: `http://52.53.171.51:3004/api/restaurants/${id}/nearby`,
+        method: "GET",
+        success: (data) => {
+          this.setState({
+            currentRestaurant: data[0],
+            nearbyRestaurants: data[1],
+          })
+        },
+        error: (err) => {
+          console.log('GET Error: ', err)
+        }
+      })
+    } else {
+      this.setState({
+        checkID: false
+      })
+    }
+
+  }
 
   _goToRestaurant(id) {
     location.href = '/restaurants/' + id;
   }
 
-	render() {
+  render() {
     let restaurantCards = this.state.nearbyRestaurants.map((num, index) => {
       return (
         <RestaurantCard nearbyRestaurant={this.state.nearbyRestaurants[index]} key={index.toString()} switchRestaurant={this._goToRestaurant.bind(this)} />
       )
     })
 
-		return (
-			<div className="nearby-padding">
-				<div className="restaurant-header">Restaurants Near {this.state.currentRestaurant.name ? this.state.currentRestaurant.name : "none"}</div>
+    return (
+      <div className="nearby-padding">
+        <div className="restaurant-header">Restaurants Near {this.state.currentRestaurant.name ? this.state.currentRestaurant.name : "none"}</div>
         <div className="restaurant-cards">
-				{restaurantCards}
+          {restaurantCards}
         </div>
         <Footer />
-			</div>
-		)
-	}
+      </div>
+    )
+  }
 }
 
 export default App;
